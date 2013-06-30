7b07b8747e8905b40e74b1b569220f2f886338d2
3bl3gamer <3bl3gamer@gmail.com>
2013-07-01 02:51:14 +0400

    Небольшой фикс сервера

--- server.js
+++ server.js
@@ -6,8 +6,9 @@ var url = require('url');
 
 var fileServer = new static.Server('.');
 
-var img_count = 0;
 var img_folder = "img/";
+var img_count = fs.readdirSync(img_folder).length;
+console.log("Found "+img_count+" already saved image(s).");
 
 http.createServer(function (req, res) {
 	if (req.method == "GET")
@@ -19,7 +20,7 @@ http.createServer(function (req, res) {
 			var data = fs.readFileSync("index.html", "utf-8");
 			data = data.replace(
 				/\/\*server data from\*\/[\d\D]*\/\*server data to\*\//,
-				"\""+img_folder+img_count+".png\"");
+				"\""+img_folder+(img_count-1)+".png\"");
 			req.addListener('end', function () {
 				res.writeHead(200, {'Content-Type': 'text/html'});
 				res.write(data);
@@ -36,11 +37,11 @@ http.createServer(function (req, res) {
 			data += chunk;
 		}).addListener('end', function () {
 			try {
-				fs.writeFile(img_folder+(++img_count)+".png",
+				fs.writeFile(img_folder+(img_count++)+".png",
 				             data.substr(22), "base64",
 				             function (err) {
 					if (err) throw err;
-					console.log(img_count+" image(s) saved");
+					console.log(img_count+" image(s) saved.");
 				});
 			} catch(e) {
 				res.writeHead(400, {'Content-Type': 'text/plain'});
@@ -57,3 +58,4 @@ http.createServer(function (req, res) {
 		fileServer.serve(req, res);
 	});
 }).listen(9004);
+console.log("Server started.")
